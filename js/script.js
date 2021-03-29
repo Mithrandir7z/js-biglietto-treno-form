98e818c562fa0ef53d7a706b5807d74539341796
MattiaManfreda <mattiamanfreda926@gmail.com>
2021-03-29 17:19:15 +0200

    agg2

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//AL CLICK DEL BOTTONE GENERA: prendere in input il valore di NomeCognome // KmDaPercorrere
+//e prendere in input il valore di eta-utente
+
+//usare gli if per calcolare il prezzo del biglietto con o senza sconto
+
+//al click del tasto genera stampa tutto
+//al click del tasto cancella stampa valore '' a tutto
+
+
+
+
+//AL CLICK DEL BOTTONE GENERA: button-genera // KmDaPercorrere
+//e prendere in input il valore di eta-utente
+var generaInput = document.getElementById("button-genera");
+
+generaInput.addEventListener("click", function(){
+
+    //Prendere in input il valore di NomeCognome
+    var NomeCognome = document.getElementById("nome-cognome-input").value;
+
+    //Prendere in input il valore di km
+    var chilometriUtente = document.getElementById("km-da-percorrere-input").value;
+
+    //Prendere in input il valore di eta-utente e settare degli if per verificare l'età, 
+    //dopo di che calcolare lo sconto.
+    
+});
